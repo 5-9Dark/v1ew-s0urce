0861d7d2a5998be6153001532e51efd0189b6191
CRO-THEHACKER <crothehacker@fiveninedark.com>
2020-07-27 19:36:59 -0400

    Update main.py

--- src/main/main.py
+++ src/main/main.py
@@ -8,4 +8,32 @@
 # /       \  / /      /    / |    `   \/ __ \|  | \/    < 
 #/______  / / /      /____/ /_______  (____  /__|  |__|_ \
 #       \/  \/                      \/     \/           \/
-#  - Made with <3 by the FiveNineDark Team  
+#  - Made with <3 by the FiveNineDark Dev team  
+
+
+import os
+import sys
+import random
+import time
+
+class color:
+    HEADER = '\033[95m'
+    IMPORTANT = '\33[35m'
+    NOTICE = '\033[33m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+    UNDERLINE = '\033[4m'
+    LOGGING = '\33[34m'
+
+color_random=[color.HEADER,color.IMPORTANT,color.NOTICE,color.OKBLUE,color.OKGREEN,color.WARNING,color.RED,color.END,color.UNDERLINE,color.LOGGING]
+random.shuffle(color_random)
+
+def clearScr():
+    os.system('clear')
+
+####################
+
+
